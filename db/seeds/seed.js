eed36ff5ec91560e5870c55ec30924f72dc6c3cf
Liam Perkins <liam.perkins.1992@gmail.com>
2020-09-21 17:17:00 +0100

    seed.js file reformatted

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -8,24 +8,21 @@ const {
 const {createTimeStamp} = require('../utils/data-manipulation')
 
 exports.seed = function (knex) {
-  return knex.migrate
-  .rollback()
+  return knex.migrate.rollback()
   .then(()=> {
    return knex.migrate.latest()
-  }).then(()=>{
-    return knex('topics')
-    .insert(topicData)
-    .returning('*')
-    .then(()=>{
-      return knex('users')
-      .insert(userData)
-      .returning('*')
-      .then(()=> {
-        const articlesWithAdaptedTimeStamp = createTimeStamp(articleData)
-        return knex.insert(articlesWithAdaptedTimeStamp)
-        .into('articles')
-        .returning('*')
-      })
-    })
   })
+  .then(()=>{
+   return knex('topics').insert(topicData).returning('*')
+  })
+  .then(()=>{
+   return knex('users').insert(userData).returning('*')
+  })
+  .then(()=> {
+   const articlesWithAdaptedTimeStamp = createTimeStamp(articleData)
+   return knex.insert(articlesWithAdaptedTimeStamp).into('articles').returning('*')
+  })
+  .catch(err => {
+   console.log(err)
+  }) 
 };
