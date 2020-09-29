c95bf26634a5cfdbc9f5773c4d93aae925d6b48e
jonp2020 <jon.parnham@gmail.com>
2020-09-29 12:41:40 +0100

    updated app, listen, connection and package.json files

--- api/app.js
+++ api/app.js
@@ -4,15 +4,13 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./Router/api.router");
 const {
-  handleCustomErrors,
-  handleInvalidPath,
-  handle400s,
+	handleCustomErrors,
+	handleInvalidPath,
+	handle400s,
 } = require("./errors");
 
 app.use(express.json());
-
 app.use("/api", apiRouter);
-
 app.all("/*", handleInvalidPath);
 app.use(handle400s);
 app.use(handleCustomErrors);
--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,14 @@
-const config = require("../knexfile");
-const knex = require("knex")(config);
+// const config = require("../knexfile");
+// const knex = require("knex")(config);
 
-module.exports = knex;
+// module.exports = knex;
+
+const ENV = process.env.NODE_ENV || 'development';
+const knex = require('knex');
+
+const dbConfig =
+	ENV === 'production'
+		? { client: 'pg', connection: process.env.DATABASE_URL }
+		: require('../knexfile');
+
+module.exports = knex(dbConfig);
