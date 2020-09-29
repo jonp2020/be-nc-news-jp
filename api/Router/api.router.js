2c9c76013a7ecfb6c8e364645320ecfb29e1a0eb
jonp2020 <jon.parnham@gmail.com>
2020-09-29 15:52:07 +0100

    added description of endpoints on api/ endpoint

--- api/Router/api.router.js
+++ api/Router/api.router.js
@@ -3,6 +3,21 @@ const topicsRouter = require("./topics.router");
 const usersRouter = require("./users.router");
 const articlesRouter = require("./articles.router");
 const commentsRouter = require("./comments.router");
+const fs = require("fs");
+
+apiRouter.route("/").get((req, res, next) => {
+  // const apiContent = [];
+  return fs.promises
+    .readFile("../endpoints.json", "utf8")
+    .then((apiContent) => {
+      console.log("content--- ", apiContent);
+      res.status(200).send(apiContent);
+    })
+    .catch((err) => {
+      console.log("err: ", err);
+      next(err);
+    });
+});
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
